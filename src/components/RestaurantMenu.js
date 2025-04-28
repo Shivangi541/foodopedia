107bc84deae6f7e0d4eda697501704cbd33f0cb8
Shivangi541 <shivangidutta68@gmail.com>
2025-04-29 00:21:47 +0530

    creating a restaurant menu card

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,34 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 const RestaurantMenu = () => {
+  const [menu, setMenu] = useState(null); // State to store menu data
   useEffect(() => {
-    fetchMenuData();
+    fetchMenu();
   }, []);
-  // Fetch restaurant menu data from API
-  const fetchMenuData = async () => {
-    try {
-      const response = await fetch(
-        "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5812837&lng=88.4261513&restaurantId=10370&catalog_qa=undefined&submitAction=ENTER"
-      );
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error("Error fetching menu data:", error);
-    }
+
+  const fetchMenu = async () => {
+    const response = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5812837&lng=88.4261513&restaurantId=10370&catalog_qa=undefined&submitAction=ENTER"
+    );
+    const result = await response.json();
+    setMenu(result.data); // Set the menu data to state
+    console.log(result);
   };
-
-  fetchMenuData();
-
+  if (menu === null) {
+    return <Shimmer />;
+  }
+  const { name, cuisines, cloudinaryImageId, costForTwoMessage, areaName } =
+    menu?.cards[2]?.card?.card?.info;
   return (
-    <div>
-      <h1>Restaurant Menu</h1>
-      <p>Here you can find the menu of our restaurant.</p>
+    <div className="menu-item">
+      <h1>{name}</h1>
+      <h3>{cuisines.join(", ")}</h3>
+      <h3>{areaName}</h3>
+      <h3>{costForTwoMessage}</h3>
+      <p>Menu details will be displayed here.</p>
       <ul>
-        <li>Pizza</li>
-        <li>Burger</li>
-        <li>Pasta</li>
-        <li>Salad</li>
-        <li>Dessert</li>
-        <li>Drinks</li>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+        <li>Item 5</li>
+        <li>Item 6</li>
+        <li>Item 7</li>
+        <li>Item 8</li>
+        <li>Item 9</li>
       </ul>
     </div>
   );
