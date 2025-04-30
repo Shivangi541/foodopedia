683a1ac54253f948588cfc27f88019491a42ceb4
Shivangi541 <shivangidutta68@gmail.com>
2025-04-30 22:55:04 +0530

    added menu api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -10,3 +10,6 @@ export const RESTAURANT_API =
 
 export const RESTAURANT_IMG =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660";
+
+export const MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5812837&lng=88.4261513&restaurantId=";
