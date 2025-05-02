e404ab2caddf1666c957001490c38ab76f8c01c5
Shivangi541 <shivangidutta68@gmail.com>
2025-05-02 09:51:55 +0530

    learning more aout class based components

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,38 @@
-import User from "./User";
+//import User from "./User";
 import UserClass from "./UserClass";
-const About = () => {
-  return (
-    <div>
-      <h1>About Us</h1>
-      <p>This is a simple React application.</p>
-      <p>We are learning React Router.</p>
-      <User />
-      <UserClass name={"shivangi dutta"} />
-    </div>
-  );
-};
+import { Component } from "react";
+class About extends Component {
+  constructor(props) {
+    super(props);
+    console.log("parent constructor");
+  }
+  componentDidMount() {
+    console.log("parent componentDidMount");
+  }
+  render() {
+    console.log("parent render");
+    return (
+      <div>
+        <h1>About Us</h1>
+        <p>This is a simple React application.</p>
+        <p>We are learning React Router.</p>
+
+        <UserClass name={"shivangi dutta"} />
+        <UserClass name={"shivangi dutta2"} />
+      </div>
+    );
+  }
+}
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>About Us</h1>
+//       <p>This is a simple React application.</p>
+//       <p>We are learning React Router.</p>
+
+//       <UserClass name={"shivangi dutta"} />
+//     </div>
+//   );
+// };
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -2,13 +2,33 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
+    this.state = {
+      name: "User",
+      count: 0,
+    };
+    console.log("child constructor");
+  }
+  componentDidMount() {
+    console.log("child componentDidMount");
   }
   render() {
+    const { name } = this.props;
+    console.log("child render");
     return (
       <div>
         <h1>User Class Component</h1>
-        <p>{this.props.name}</p>
+        <p>{name}</p>
+        <p>{this.state.count}</p>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          Click me
+        </button>
+        <p>{this.state.name}</p>
         <p>This is a simple React application.</p>
         <p>We are learning React Router.</p>
       </div>
